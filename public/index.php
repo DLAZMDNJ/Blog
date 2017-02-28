4f6cda2865e067bae4b01aa6c2f2e1b4047c018b
anjin <anjin@192.168.0.101>
2017-02-28 16:46:53 +0800

    1

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
-echo 'aaa';
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
@@ -19,7 +19,7 @@ echo 'aaa';
 |
 */
 
-// require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ echo 'aaa';
 |
 */
 
-// $app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -47,12 +47,12 @@ echo 'aaa';
 |
 */
 
-// $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-// $response = $kernel->handle(
-//     $request = Illuminate\Http\Request::capture()
-// );
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-// $response->send();
+$response->send();
 
-// $kernel->terminate($request, $response);
+$kernel->terminate($request, $response);
