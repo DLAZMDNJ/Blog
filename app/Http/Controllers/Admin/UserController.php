918738518ddb9e29b2bf9407734560345902516b
anjin <anjin@192.168.0.101>
2017-03-09 10:40:36 +0800

    1

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -27,4 +27,22 @@ class UserController extends Controller
     	return view('admin.user.index',['data'=> $data]);
     	
     }
+    public function edit($id)
+    {
+    	$userinfo = DB::table('blog_user')->where('id',$id)->first();
+    	return view('admin.user.edit',['userinfo' => $userinfo]);
+    }
+    public function doEdit(Request $request)
+    {
+    	$data = $request->except('_token');
+    	DB::table('blog_user')->where('id',$data['id'])->update($data);
+    	$data = DB::table('blog_user')->get();
+    	return view('admin.user.index',['data'=> $data]);
+    }
+    public function delete($id)
+    {
+    	DB::table('blog_user')->where('id',$id)->delete();
+    	$data = DB::table('blog_user')->get();
+    	return view('admin.user.index',['data'=>$data]);
+    }
 }
--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -3,21 +3,28 @@
 		 <div id="page-wrapper">
 
             <div class="container-fluid">
-	
 			<div class="row">
-	
+                    <div class="col-lg-3">
 
-                    <div class="col-lg-6">
+                        <form role="form" method="post" action="{{asset("/admin/user/doEdit")}}">
+                       	<input type="hidden" name="_token" value="{{ csrf_token() }}">
+                       	<input type="hidden" name="id" value="{{$userinfo->id}}">
 
-                        <form role="form" method="post" action="{{asset("/admin/message/doEdit")}}">
-                       <input type="hidden" name="_token" value="{{ csrf_token() }}">
-							<input type="hidden" name="id" value="{{$data->id}}">
                             <div class="form-group">
-                                <label>兴趣名称</label>
-                                <input class="form-control" name="name" value="{{$data->name}}">
+                                <label>用户名</label>
+                                <input class="form-control" name="name" value="{{$userinfo->name}}">
+                                <p class="help-block">6-18位.</p>
+                            </div>
+                            <div class="form-group">
+                                <label>昵称</label>
+                                <input class="form-control" name="nickname" value="{{$userinfo->nickname}}">
                                 <p class="help-block">5-10个汉字.</p>
                             </div>
-
+							<div class="form-group">
+                                <label>用户密码</label>
+                                <input class="form-control" name="password" value="{{$userinfo->password}}" type="password">
+                                <p class="help-block">6-18位.</p>
+                            </div>
 <!--                             <div class="form-group"> -->
 <!--                                 <label>备用输入框，后期使用</label> -->
 <!--                                 <input class="form-control" placeholder="Enter text"> -->
@@ -29,15 +36,14 @@
 	<!--                             </div> -->
 
                             <div class="form-group">
-                                <label>图片</label>
-                                <input type="file" name="pic">
-                                
+                                <label>用户头像</label>
+                                <input type="file" name="pic" >
                             </div>
-							<img src="{{$data->pic}}" width="100px;">
-                            <div class="form-group">
-                                <label>兴趣描述</label>
-                                <textarea class="form-control" rows="3" name="dic">{{$data->dic}}</textarea>
-                            </div>
+							<img src="{{$userinfo->name}}" width="100px;">
+<!--                             <div class="form-group"> -->
+<!--                                 <label>兴趣描述</label> -->
+<!--                                 <textarea class="form-control" rows="3" name="dic"></textarea> -->
+<!--                             </div> -->
 
 <!--                             <div class="form-group"> -->
 <!--                                 <label>Checkboxes</label> -->
@@ -103,17 +109,38 @@
 <!--                                 </label> -->
 <!--                             </div> -->
 
-<!--                             <div class="form-group"> -->
-<!--                                 <label>Selects</label> -->
-<!--                                 <select class="form-control"> -->
-<!--                                     <option>1</option> -->
-<!--                                     <option>2</option> -->
-<!--                                     <option>3</option> -->
-<!--                                     <option>4</option> -->
-<!--                                     <option>5</option> -->
-<!--                                 </select> -->
-<!--                             </div> -->
-
+                            <div class="form-group">
+                                <label>用户状态</label>
+                                <select class="form-control" name="status">
+                                    <option value="0" 
+                                    @if($userinfo->status == 0)
+                                    selected
+                                    @endif
+                                    >正常</option>
+                                    <option value="1"
+                                    @if($userinfo->status == 1)
+                                    selected
+                                    @endif
+                                    
+                                    
+                                    >冻结</option>
+                                </select>
+                            </div>
+							 <div class="form-group">
+                                <label>用户角色</label>
+                                <select class="form-control" name="auth">
+                                    <option value="0"
+                                    @if($userinfo->auth == 0)
+                                    selected
+                                    @endif                             
+                                    >管理员</option>
+                                    <option value="1"
+                                    @if($userinfo->auth == 1)
+                                    selected
+                                    @endif
+                                    >普通用户</option>
+                                </select>
+                            </div>
 <!--                             <div class="form-group"> -->
 <!--                                 <label>Multiple Selects</label> -->
 <!--                                 <select multiple class="form-control"> -->
@@ -125,7 +152,7 @@
 <!--                                 </select> -->
 <!--                             </div> -->
 
-                            <button type="submit" class="btn btn-default">修改</button>
+                            <button type="submit" class="btn btn-default">提交</button>
                             <button type="reset" class="btn btn-default">重置</button>
 
                         </form>
