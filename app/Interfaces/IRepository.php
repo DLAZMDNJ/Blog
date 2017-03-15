58d3b3eeed9ceca72a06b5bf63c04bf6050d32fc
anjin <anjin@192.168.0.101>
2017-03-15 15:02:54 +0800

    1

---
+++ app/Interfaces/IRepository.php
@@ -0,0 +1,30 @@
+<?php 
+namespace  APP\Interfaces;
+
+interface IRepository
+{
+	public function index($data,$extra)
+	{
+		
+	}
+	public function add()
+	{
+		
+	}
+	public function doAdd($data,$extra)
+	{
+		
+	}
+	public function edit($id,$extra)
+	{
+		
+	}
+	public function doEdit($data,$extra)
+	{
+		
+	}
+	public function delete($id,$extra)
+	{
+		
+	}
+}
