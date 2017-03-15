b8d5fd2dc06e456f3db11d931856c5f645704498
anjin <anjin@192.168.0.101>
2017-03-15 16:14:15 +0800

    1

---
+++ app/Repositories/SunRepository.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App\Repositories;
+use App\Models\friends;
+use Illuminate\Http\Request;
+use DB;
+
+class SunRepository extends BaseRepository
+{
+	protected $model;
+	public function index($data, $extra='')
+	{
+		
+	}
+}
