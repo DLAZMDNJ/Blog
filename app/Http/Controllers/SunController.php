fe42dab5ccf3f53e7c262dbdd4bc44ae06a48483
anjin <anjin@192.168.0.101>
2017-03-16 15:43:33 +0800

    1

--- app/Http/Controllers/SunController.php
+++ app/Http/Controllers/SunController.php
@@ -8,20 +8,17 @@ use App\Http\Requests;
 use Faker\Provider\Base;
 
 class SunController extends BaseController
-{
-    //
+{	
+	protected $repo;
+	protected $mn;
 	public function __construct()
 	{
-	
+		
+		
 	}
 	protected function repo(){
 		return $this->repo;
 	}
-	//项目名
-	protected function mis(){
-		return 'mis';
-	}
-	
 	//模块名
 	protected function mn(){
 		return $this->repo()->mn();
