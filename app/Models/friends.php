af31ea6c8b3db2e8403bc98d79cf1b448e93fab5
anjin <anjin@192.168.0.101>
2017-03-16 17:07:56 +0800

    1

--- app/Models/friends.php
+++ app/Models/friends.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-use Zizaco\Entrust\Traits\EntrustUserTrait;
+
 use DB;
 
 /**
@@ -18,8 +18,7 @@ use DB;
 class User extends sun implements AuthenticatableContract, CanResetPasswordContract
 {
 
-	use Authenticatable, CanResetPassword;
-	use EntrustUserTrait;
+	
 	protected $table = 'blog_friends';
 	public function mn(){
 		return 'user';
