4859256dcd24bf3f3ff66fe1e348e0e85d2b59fb
anjin <anjin@192.168.0.104>
2017-04-15 10:35:02 +0800

    12

--- resources/views/hotel/admin/qm_food/add.blade.php
+++ resources/views/hotel/admin/qm_food/add.blade.php
@@ -14,27 +14,15 @@
                                 <input class="form-control" name="name">
                             </div>
 							<div class="form-group">					
-                                <label>房间类型</label>
+                                <label> 餐饮类型</label>
                                 <select class="form-control" name="cid">
                                 @foreach($cate as $v)
                                     <option value="{{$v->id}}">{{$v->name}}</option>
                                  @endforeach   
                                 </select>
                             </div> 
-                            <div class="form-group">
-                                <label>是否有早餐</label>
-                                <select class="form-control" name="is_food">
-                                    <option value="1">是</option>
-                                    <option value="2">否</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label>是否已下架</label>
-                                <select class="form-control" name="is_onsale">
-                                    <option value="1">是</option>
-                                    <option value="2">否</option>
-                                </select>
-                            </div>
+                      
+                           
                              <div class="form-group">
                                 <label>是否已保洁</label>
                                 <select class="form-control" name="is_clean">
